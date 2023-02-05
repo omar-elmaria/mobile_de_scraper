e6d75cd0341d5b41ed44b8c8688eb2f92f8ed99c
Omar ElMaria <omar.elmaria@deliveryhero.com>
2023-02-05 10:16:15 +0100

    add main.py scripts

---
+++ main_cat_2.py
@@ -0,0 +1,12 @@
+from mobile_de_selenium_code_prod_local_single_func import mobile_de_local_single_func
+
+def main():
+    mobile_de_local_single_func(
+        category="cat_2",
+        car_list=[
+            "Audi"
+        ]
+    )
+
+if __name__ == '__main__':
+    main()
