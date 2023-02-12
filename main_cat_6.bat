8af66adca71bc588be600a9807e96144d5cc38a7
Omar ElMaria <omar.elmaria@deliveryhero.com>
2023-02-12 22:07:35 +0100

    add bat files and update gitignore

---
+++ main_cat_6.bat
@@ -0,0 +1,1 @@
+"I:\My Drive\AUC Drive\Entrepreneurial Path and Financial Freedom\Freelancing\scraping_gigs\venv_scraping_lenovo\Scripts\python.exe" "I:\My Drive\AUC Drive\Entrepreneurial Path and Financial Freedom\Freelancing\scraping_gigs\lukas_mobile_de_crawling\main_cat_6.py"
