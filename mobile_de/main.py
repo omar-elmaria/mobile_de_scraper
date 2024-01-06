784b19a16ab24fc9d281cea1ac3c06c241b9880c
Omar ElMaria <omar.elmaria@mim2018.esmt.org>
2024-01-06 21:08:16 +0100

    add zyte crawler for listing pge

--- mobile_de/main.py
+++ mobile_de/main.py
@@ -18,6 +18,8 @@ from mobile_de_selenium_code_prod_listing_page_func import (
 from scrapy.crawler import CrawlerProcess
 
 from mobile_de.spiders.mobile_de_zyte_api_car_page_spider import CarPageSpider
+from mobile_de.spiders.mobile_de_zyte_api_listing_page_spider import ListingPageSpider
+from inputs import listing_page_crawling_framework, marke_list, modell_list
 
 crawl_now = False
 def is_between_time_range():
@@ -43,334 +45,40 @@ def main():
     gdrive_folder_id="16e4f41zhwV67Pm01I0jHwL2l59kpn8WY"
 
     # Run the Selenium script that crawls the listing page
-    mobile_de_local_single_func(
-        category="cat_all",
-        car_list=[
-            # Cat 1
-            "ALPINA",
-            "Aston Martin",
-            "Bentley",
-            "Bugatti",
-            "Ferrari",
-            "Koenigsegg",
-            "KTM",
-            "Lamborghini",
-            "McLaren",
-            "Pagani",
-            "Rolls-Royce",
-            "Wiesmann",
+    if listing_page_crawling_framework == "selenium":
+        # Print a status message indicating the start of the selenium script
+        logging.info("Running the selenium crawler to get the listing page URLs...")
+        
+        # Run the selenium script
+        mobile_de_local_single_func(
+            category="cat_all",
+            car_list=marke_list,
+            modell_list=modell_list,
+            captcha_solver_default="capmonster"
+        )
 
-            # Cat 2
-            "Audi",
+        # Print a status message indicating the end of the selenium script
+        logging.info("The Selenium script finished running. Now, running the Scrapy spider...")
+    elif listing_page_crawling_framework == "zyte":
+        # Print a status message indicating the start of the zyte spider
+        logging.info("Running the Zyte crawler to get the listing page URLs...")
 
-            # Cat 3
-            "Mercedes-Benz",
+        # Run the zyte listing page spider
+        process1 = CrawlerProcess()
+        process1.crawl(ListingPageSpider)
+        process1.start()
 
-            # Cat 4
-            "BMW",
+        # Print a status message indicating the end of the zyte spider
+        logging.info("The Selenium script finished running. Now, running the Scrapy spider...")
 
-            # Cat 5
-            "Corvette",
-            "Dodge",
-            "Nissan",
-            "Ford",
-            "Alfa Romeo",
-            "Jaguar",
-            "Lexus",
-            "Lotus",
-            "Maserati",
-            "Honda",
 
-            # Cat 6
-            "Porsche"
-        ],
-        modell_list=[
-            # Cat 1
-            # ALPINA
-            "Andere",
-
-            # Aston Martin
-            "DB",
-            "DB11",
-            "DB9",
-            "DBS",
-            "DBX",
-            "Rapide",
-            "V12 Vantage",
-            "V8 Vantage",
-            "Vanquish",
-            "Virage",
-            "Andere",
-
-            # Bentley
-            "Bentayga",
-            "    Continental",
-            "    Continental Flying Spur",
-            "    Continental GT",
-            "    Continental GTC",
-            "    Continental Supersports",
-            "Flying Spur",
-            "Andere",
-
-            # Bugatti
-            "Chiron",
-            "EB 110",
-            "Veyron",
-            "Andere",
-
-            # Ferrari
-            "296 GTB",
-            "360",
-            "365",
-            "458",
-            "488 GTB",
-            "488 Pista",
-            "488 Spider",
-            "550",
-            "575",
-            "599 GTB",
-            "599 GTO",
-            "599 SA Aperta",
-            "612",
-            "812",
-            "California",
-            "Enzo Ferrari",
-            "F12",
-            "F355",
-            "F40",
-            "F430",
-            "F50",
-            "F8",
-            "FF",
-            "LaFerrari",
-            "GTC4Lusso",
-            "Portofino",
-            "Purosangue",
-            "Roma",
-            "SF90",
-            "Superamerica",
-            "Testarossa",
-            "Andere",
-
-            # Koenigsegg
-            "Agera",
-            "CCR",
-            "CCXR",
-            "Andere",
-
-            # KTM
-            "X-BOW",
-            "Andere",
-
-            # Lamborghini
-            "Aventador",
-            "Countach",
-            "Diablo",
-            "Gallardo",
-            "Huracán",
-            "Jalpa",
-            "Murciélago",
-            "Urus",
-            "Andere",
-
-            # McLaren
-            "540C",
-            "570GT",
-            "570S",
-            "600LT",
-            "620R",
-            "650S",
-            "650S Coupé",
-            "650S Spider",
-            "675LT",
-            "675LT Spider",
-            "720S",
-            "765LT",
-            "Artura",
-            "Elva",
-            "GT",
-            "MP4-12C",
-            "P1",
-            "Senna GTR",
-            "Speedtail",
-            "Andere",
-
-            # Pagani
-            "Huayra",
-            "Zonda",
-            "Andere",
-
-            # Rolls-Royce
-            "Flying Spur",
-            "Corniche",
-            "Cullinan",
-            "Dawn",
-            "Phantom",
-            "Silver Cloud",
-            "Silver Dawn",
-            "Silver Seraph",
-            "Ghost",
-            "Silver Shadow",
-            "Silver Spirit",
-            "Silver Spur",
-            "Wraith",
-            "Andere",
-
-            # Wiesmann
-            "MF 3",
-            "MF 35",
-            "MF 4",
-            "MF 5",
-
-            ###---------------###
-
-            # Cat 2
-            # Audi
-            "e-tron",
-            "quattro",
-            "R8",
-            "RS2",
-            "RS3",
-            "RS4",
-            "RS5",
-            "RS6",
-            "RS7",
-            "RSQ3",
-            "RSQ8",
-            "S8",
-            "    TT RS",
-
-            ###---------------###
-
-            # Cat 3
-            # Mercedes-Benz
-            "    A 45 AMG",
-            "    AMG GT",
-            "    AMG GT C",
-            "    AMG GT R",
-            "    AMG GT S",
-            "    C 63 AMG",
-            "    CL 65 AMG",
-            "    CLA 45 AMG",
-            "    CLA 45 AMG Shooting Brake",
-            "    CLK 63 AMG",
-            "    CLS 63 AMG",
-            "    CLS 63 AMG Shooting Brake",
-            "    E 63 AMG",
-            "    G 63 AMG",
-            "    G 65 AMG",
-            "    GL 63 AMG",
-            "    GLA 45 AMG",
-            "    GLC 63 AMG",
-            "    GLE 63 AMG",
-            "    GLS 63",
-            "    ML 63 AMG",
-            "    S 63 AMG",
-            "    S 65 AMG",
-            "    SL 63 AMG",
-            "    SL 65 AMG",
-            "SLS AMG",
-            "SLR",
-
-            ###---------------###
-
-            # Cat 4
-            # BMW
-            "    1er M Coupé",
-            "    M2",
-            "    M3",
-            "    M4",
-            "    M5",
-            "    M6",
-            "    M8",
-            "    X3 M",
-            "    X4 M",
-            "    X5 M",
-            "    X6 M",
-            "    Z3 M",
-            "    Z4 M",
-            "    Z8",
-
-            ###---------------###
-
-            # Cat 5
-            # Corvette
-            "C8",
-            
-            # Dodge
-            "Viper",
-
-            # Nissan
-            "GT-R",
-            
-            # Ford
-            "GT",
-
-            # Alfa Romeo
-            "4C",
-            "8C",
-
-            # Jaguar
-            "F-Type",
-            "Andere",
-
-            # Lexus
-            "LFA",
-
-            # Lotus
-            "Exige",
-            "Emira",
-            "Andere",
-
-            # Maserati
-            "MC20",
-
-            # Honda
-            "NSX",
-
-            ###---------------###
-
-            # Cat 6
-            # Porsche
-            "356",
-            "912",
-            "914",
-            "918",
-            "924",
-            "928",
-            "    930",
-            "944",
-            "959",
-            "962",
-            "    964",
-            "968",
-            "    991",
-            "    992",
-            "    993",
-            "    996",
-            "    997",
-            "Boxster",
-            "Carrera GT",
-            "Cayenne",
-            "Cayman",
-            "Macan",
-            "Panamera",
-            "Taycan",
-            "Andere",
-        ],
-        captcha_solver_default="capmonster"
-    )
+    # Run the car page spider
+    process2 = CrawlerProcess()
+    process2.crawl(CarPageSpider)
+    process2.start()
 
     # Print a status message
-    logging.info("The Selenium script finished running. Now, running the Scrapy spider...")
-
-    # Run the spider
-    process = CrawlerProcess()
-    process.crawl(CarPageSpider)
-    process.start()
-
-    # Print a status message
-    logging.info("The Scrapy spider finished running. Now, cleaning the data...")
+    logging.info("The Scrapy spider that crawls the car pages finished running. Now, cleaning the data...")
 
     # Retrieve the output data from the JSON file
     with open("df_all_brands_data_cat_all.json", mode="r", encoding='utf-8') as f:
