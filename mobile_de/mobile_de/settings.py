2a8fbe7ae942184c31bad5e3545bdab87ef1f4bb
Omar ElMaria <omar.elmaria@mim2018.esmt.org>
2024-01-11 22:18:48 +0100

    return back settings.py to its original state

--- mobile_de/mobile_de/settings.py
+++ mobile_de/mobile_de/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'mobile_de.spiders'
 #USER_AGENT = 'mobile_de (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-# ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
